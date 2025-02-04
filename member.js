d179bafb2a6a84874b6b69ade0e2e7b52795b009
Guilherme Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2025-02-04 22:08:19 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,14 @@
-function skillMember() {
-    console.log('Skill: Member');
+function skillsMember() {
+    var member = {
+        name: 'Jhon',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        details: function() {
+            skills = this.skills;
+            skills.forEach((skill) => {
+                console.log(`${this.name} knows ${skill}`);
+            });
+        }
+    }
+    return member;
 }
